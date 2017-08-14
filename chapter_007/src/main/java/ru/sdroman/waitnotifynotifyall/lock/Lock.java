47df600ab4600e288a9fdc7c6db9557ca93d039c
Sedykh Roman <sedykhroman@gmail.com>
2017-08-14 21:59:49 +0300

    3. Реализовать собственный механизм блокировок Lock. [#1100] Review_01

--- chapter_007/src/main/java/ru/sdroman/waitnotifynotifyall/lock/Lock.java
+++ chapter_007/src/main/java/ru/sdroman/waitnotifynotifyall/lock/Lock.java
@@ -1,8 +1,37 @@
 package ru.sdroman.waitnotifynotifyall.lock;
 
 /**
+ * Class Lock.
+ *
  * @author sdroman
  * @since 08.2017
  */
 public class Lock {
+
+    /**
+     * isLocked.
+     */
+    private boolean isLocked;
+
+    /**
+     * Lock.
+     */
+    public synchronized void lock() {
+        while (isLocked) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.isLocked = true;
+    }
+
+    /**
+     * Unlock.
+     */
+    public synchronized void unlock() {
+        this.isLocked = false;
+        notifyAll();
+    }
 }
