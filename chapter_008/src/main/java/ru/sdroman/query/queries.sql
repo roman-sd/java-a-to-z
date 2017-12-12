c5e5e1a435b56e9c09fac21f4fdf234f4f5e5ca5
Sedykh Roman <sedykhroman@gmail.com>
2017-12-12 22:39:36 +0300

    Фильтры [#1732]_review_01

--- chapter_008/src/main/java/ru/sdroman/query/queries.sql
+++ chapter_008/src/main/java/ru/sdroman/query/queries.sql
@@ -1,28 +1,30 @@
-SELECT p.id, p.name, to_char(p.expired_date, 'dd/mm/yy') AS expired_date, t.name AS type FROM product p, type t 
-WHERE  p.type_id = t.id AND t.name = 'cheese';
+SELECT p.name, to_char(expired_date, 'dd/mm/yy') AS "expired date", p.price, t.name AS type 
+FROM product p JOIN type t ON p.type_id = t.id
+WHERE t.name = 'cheese';
 
-SELECT p.id, p.name, to_char(p.expired_date, 'dd/mm/yy') AS expired_date, t.name AS type FROM product p, type t
-WHERE p.type_id = t.id AND p.name = 'parmalat';
+SELECT p.name, t.name AS type
+FROM product p JOIN type t ON p.type_id = t.id
+WHERE p.name like '%apple%';
 
-SELECT p.id, p.name, to_char(p.expired_date, 'dd/mm/yy') AS expired_date, t.name AS type FROM product p, type t 
-WHERE p.type_id = t.id AND date_trunc('month', CURRENT_TIMESTAMP + INTERVAL '2' MONTH) > p.expired_date;
+SELECT p.name, to_char(expired_date, 'dd/mm/yy') AS "expired date", p.price, t.name AS type 
+FROM product p JOIN type t ON p.type_id = t.id
+WHERE date_trunc('month', CURRENT_TIMESTAMP + INTERVAL '2' month) > p.expired_date;
 
 SELECT p.id, p.name, p.price, t.name FROM product p, type t
 WHERE p.type_id = t.id AND price = (SELECT max(price) FROM product);
 
-SELECT t.name, COUNT(p.id) FROM type t, product p
-WHERE p.type_id = t.id 
-GROUP BY t.id; 
+SELECT p.name, to_char(expired_date, 'dd/mm/yy') AS "expired date", p.price, t.name AS type 
+FROM product p JOIN type t ON p.type_id = t.id
+WHERE date_trunc('month', CURRENT_TIMESTAMP + INTERVAL '2' month) > p.expired_date;
 
-SELECT p.name, t.name AS type FROM product p, type t 
-WHERE p.type_id = t.id 
-AND (t.name = 'milk' OR t.name = 'cheese')
+SELECT p.name, t.name AS "type", p.num AS "number"
+FROM product p JOIN type t ON p.type_id = t.id 
+WHERE p.num < 10
 ORDER BY t.name;
 
-SELECT p.name, t.name AS type, p.num as num FROM product p, type t 
-WHERE p.type_id = t.id AND p.num < 10;
+SELECT p.name, t.name AS "type"
+FROM product p JOIN type t ON p.type_id = t.id
+WHERE t.name = 'milk' OR t.name = 'cheese';
 
-SELECT p.name, to_char(p.expired_date, 'dd/mm/yy') AS expired_date, t.name AS type 
-FROM product p, type t 
-WHERE p.type_id = t.id
-ORDER BY t.name;
+SELECT p.id, p.name, t.name AS type, to_char(p.expired_date, 'dd/mm/yy') AS "expiried date", p.num AS "number"
+FROM product p JOIN type t ON p.type_id = t.id;
